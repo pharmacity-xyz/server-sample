1aec68baa73070e7493a9ee5af29a6bff08d740b
Aoi1011 <ht.so.ik0102@gmail.com>
2022-10-02 11:19:50 +0700

    Update

--- Controllers/PizzaController.cs
+++ Controllers/PizzaController.cs
@@ -32,8 +32,47 @@ public class PizzaController : ControllerBase
     }
 
     // POST action
+    [HttpPost]
+    public IActionResult Create(PizzaModel pizza)
+    {
+        // will save the pizza and return a result
+        PizzaService.Add(pizza);
+        return CreatedAtAction(nameof(Create), new { id = pizza.Id }, pizza);
+    }
 
     // PUT action
+    [HttpPut("{id}")]
+    public IActionResult Update(int id, PizzaModel pizza)
+    {
+        if (id != pizza.Id)
+        {
+            return BadRequest();
+        }
+
+        var existingPizza = PizzaService.Get(id);
+        if (existingPizza is null)
+        {
+            return NotFound();
+        }
+
+        PizzaService.Update(pizza);
+
+        return NoContent();
+    }
 
     // DELETE action
+    [HttpDelete("{id}")]
+    public IActionResult Delete(int id)
+    {
+        var pizza = PizzaService.Get(id);
+
+        if (pizza is null)
+        {
+            return NotFound();
+        }
+
+        PizzaService.Delete(id);
+
+        return NoContent();
+    }
 }
